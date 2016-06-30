06f4dff849cceac7b5a6d5fd401db92e5ccaa14b
sandiprb <sandip@coverfoxmail.com>
2016-06-30 08:02:08 +0530

    fixes

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,17 @@ var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
 var connect = require('gulp-connect');
 var atImport = require('postcss-import');
+var assets  = require('postcss-assets');
 
 gulp.task('build', function() {
 	var processors = [
 	require('postcss-mixins'),
 	atImport({
 		path: ['static/pcss']
-	}),
+		}),
 	require('postcss-simple-vars'),
 	require('postcss-nested'),
+	assets({loadPaths: ['static/images/']}),
 	require('css-mqpacker'),
 	autoprefixer({browsers: ['last 2 version']}),
 	cssnano()
@@ -27,11 +29,9 @@ gulp.task('build', function() {
 gulp.task('connect', function() {
 	connect.server({
 		livereload: true
-	});
-	});
-
-gulp.task('watch:pcss', function () {
-	gulp.watch('./static/pcss/*.pcss', ['build']);
+		});
 	});
 
-gulp.task('default', ['watch:pcss', 'connect']);
+gulp.task('default', ['build', 'connect'] , function () {
+	gulp.watch(['./static/pcss/*.css', './*.html'], ['build']);
+	});

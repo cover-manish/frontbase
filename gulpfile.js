9a4e3631e4078bdec21a58dd0d94dd170097f2e1
sandiprb <sandip@coverfoxmail.com>
2016-07-09 13:41:12 +0530

    fixed pccs partial import build,

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,32 @@
 var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
+var assets = require('postcss-assets');
+var rename = require('gulp-rename');
 var cssnano = require('cssnano');
 var connect = require('gulp-connect');
+var postcss = require('gulp-postcss');
 var atImport = require('postcss-import');
-var assets  = require('postcss-assets');
+var autoprefixer = require('autoprefixer');
 
+//build task
 gulp.task('build', function() {
 	var processors = [
 	require('postcss-mixins'),
-	atImport({
-		path: ['static/pcss']
-		}),
+	atImport({path: ['./static/pcss']}),
 	require('postcss-simple-vars'),
 	require('postcss-nested'),
 	assets({loadPaths: ['static/images/']}),
 	require('css-mqpacker'),
 	autoprefixer({browsers: ['last 2 version']}),
-	cssnano()
+	//cssnano() //uncomment when in production
 	];
-	return gulp.src('./static/pcss/*.css')
+	return gulp.src('./static/pcss/[^_]*.pcss')
 	.pipe(postcss(processors))
+	.pipe(rename({extname: '.css'}))
 	.pipe(gulp.dest('./static/css'))
 	.pipe(connect.reload())
 	});
 
-
+//livereload task
 gulp.task('connect', function() {
 	connect.server({
 		livereload: true
@@ -33,5 +34,5 @@ gulp.task('connect', function() {
 	});
 
 gulp.task('default', ['build', 'connect'] , function () {
-	gulp.watch(['./static/pcss/*.css', './*.html'], ['build']);
+	gulp.watch(['./static/pcss/*.pcss', './*.html'], ['build']);
 	});
